2583de7b7aed6b2e4785591a7dcaec36b27db8f2
Christina Moore <sayhitochristina@gmail.com>
2018-05-26 13:18:06 -0500

    homepage styles

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import SignupForm from '../components/login-signup/signupForm';
-
 import './styles/homepage.css'
+const barbell = require('../assets/barbell.png');
+const arrows = require('../assets/arrows.png');
 
 export default class HomePage extends React.Component {
   render() {
@@ -10,13 +11,20 @@ export default class HomePage extends React.Component {
       <div className='homepage-body'>
         <div className='hp-text-container'>
           <div className='main-text'>
-            <h1>Make the most of anything</h1>
-            <p>Lorem ipsum lorem lorem ipsum</p>
+            <h1>Keep track of your workouts</h1>
+            <p>to preform your best</p>
           </div>
         </div>
         <div className='homepage-signup-form'>
+          <div className='form-pair-container'>
+            <div className='form-pair'>
+              <img src={barbell} alt='null'></img>
+              <p>Record your workout</p>
+            </div>
+            <NavLink to='/login'>Login</NavLink>
+          </div>
+          <p className='form-divider'>or</p>
           <SignupForm />
-          <p>Login</p>
           <p>Signup</p>
         </div>
         <div className='homepage-picture-main'></div>
